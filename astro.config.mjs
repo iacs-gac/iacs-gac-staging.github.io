b61d3db8314d216e271cbd0f8e2532f69b73fc69
albert ge <lbertge@gmail.com>
2022-09-26 22:18:09 -0400

    Update astro.config.mjs
    Add https prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
 	// Astro uses this full URL to generate your sitemap and canonical URLs in your final build
-	site: 'iacs-gac-staging.github.io',
+	site: 'https://iacs-gac-staging.github.io',
 	base: '/iacs-gac-staging.github.io',
 
 	output: 'static',
